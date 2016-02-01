1280c9aa954c898914a23bc0e92ad94bd826a48e
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-02-01 19:37:56 +0530

    Work problem 1

--- Project2/cs142-format-time.js
+++ Project2/cs142-format-time.js
@@ -1,3 +1,13 @@
 function cs142FormatTime(date) {
-  
+  const dictDays = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday"
+  }
+  var day = dictDays[date.getDays()];
+  console.log(day);
 }
