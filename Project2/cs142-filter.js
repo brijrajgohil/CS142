0730bc809aaa78fcae907c749b5bdf12803a3d65
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-02-02 21:06:26 +0530

    Problem 3

--- Project2/cs142-filter.js
+++ Project2/cs142-filter.js
@@ -1,3 +1,7 @@
-function cs142filter() {
+function cs142filter(clientData, isOK) {
   
 }
+
+var varA = [1, 2, 3, 4, 5, 6].cs142filter(2, function (modVal, elm) {
+   return ((elm % modVal) === 0);
+});
