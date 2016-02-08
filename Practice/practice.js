6696c5c3bd5d1f5d6a8acd0feccff6f9691197f6
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-02-08 23:48:22 +0530

    Prototyping

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,1 +1,15 @@
-console.log("hello".length);
+function Person(first, last) {
+  this.first = first;
+  this.last = last;
+}
+
+Person.prototype.fullName = function () {
+  return this.first + " " + this.last;
+};
+
+Person.prototype.fullNameReversed = function () {
+  return this.last + " " + this.first;
+};
+
+var me = new Person("Brijrajsinh", "Gohil");
+console.log(me.fullName());
