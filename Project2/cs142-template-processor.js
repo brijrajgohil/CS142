0b46f0bd96159383101d583ed44b23ecdadd5777
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-02-18 18:10:35 +0530

    Update

--- Project2/cs142-template-processor.js
+++ Project2/cs142-template-processor.js
@@ -1,21 +1,19 @@
 function Cs142TemplateProcessor(st) {
-  return st
+  Cs142TemplateProcessor.prototype.fillIn = function (dict) {
+    if(st.indexOf("month") != -1) {
+      st.replace("{{month}}", dict.month);
+    }
+    if(st.indexOf("day") != -1) {
+      st.replace("{{day}}", dict.day);
+    }
+    if(st.indexOf("year") != -1) {
+      st.replace("{{year}}", dict.year);
+    }
+    return st;
+  };
 }
 
-Cs142TemplateProcessor.prototype.fillIn = function (dict) {
-  var a = String(this);
-  console.log(a);
-  if(a.indexOf("month") != -1) {
-    a.replace("{{month}}", dict.month);
-  }
-  if(a.indexOf("day") != -1) {
-    a.replace("{{day}}", dict.day);
-  }
-  if(a.indexOf("year") != -1) {
-    a.replace("{{year}}", dict.year);
-  }
-  return a;
-};
+
 
 var template = 'My favorite month is {{month}} but not the day {{day}} or the year {{year}}';
 var dateTemplate = new Cs142TemplateProcessor(template);
@@ -23,4 +21,4 @@ var dateTemplate = new Cs142TemplateProcessor(template);
 var dictionary = { month: 'July', day: '1', year: '2016'};
 var str = dateTemplate.fillIn(dictionary);
 //assert(str === 'My favorite month is July but not the day 1 or the year 2016');
-//console.log(str);
+console.log(str);
