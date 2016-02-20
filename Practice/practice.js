23761ff07f88d2e8814f2fa6d4c6d9721db32ff7
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-02-20 11:27:31 +0530

    get & set

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,15 +1,23 @@
-function Person(first, last) {
-  this.first = first;
-  this.last = last;
+function Employee(name, age) {
+  _age = age;
+  _name = name;
+  Object.defineProperty(this, "age", {
+    get : function() {
+      return _age;
+    }
+    set : function(value) {
+      if(value <1 || value >100) {
+        alert("Invalid Age");
+      }
+      else {
+        _age = value;
+      }
+    }
+  });
+
+  Object.defineProperty(this, "name", {
+    get : function() {
+      return _name;
+    }
+  });
 }
-
-Person.prototype.fullName = function () {
-  return this.first + " " + this.last;
-};
-
-Person.prototype.fullNameReversed = function () {
-  return this.last + " " + this.first;
-};
-
-var me = new Person("Brijrajsinh", "Gohil");
-console.log(me.fullName());
