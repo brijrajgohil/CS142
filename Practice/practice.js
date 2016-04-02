e6afaeac99db6f39ebef4681ce938c19b9f0d81a
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-04-02 22:56:20 +0530

    JS you don't know Up&Going Ch1

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,23 +1,29 @@
-function Employee(name, age) {
-  _age = age;
-  _name = name;
-  Object.defineProperty(this, "age", {
-    get : function() {
-      return _age;
-    }
-    set : function(value) {
-      if(value <1 || value >100) {
-        alert("Invalid Age");
-      }
-      else {
-        _age = value;
-      }
-    }
-  });
+const SPENDINGTHRESHOLD = 200;
+const TAXRATE = 0.08;
+const PHONEPRICE = 99.99;
+const ACCESSORYPRICE = 9.99;
 
-  Object.defineProperty(this, "name", {
-    get : function() {
-      return _name;
+var bankBalance = 303.91;
+var amount = 0;
+
+
+function calculateTax(amount) {
+    return amount * TAXRATE;
+}
+
+function formatAmount(amount) {
+    return "$" + amount.toFixed(2);
+}
+while(amount < bankBalance) {
+    amount += PHONEPRICE;
+    if(amount < SPENDINGTHRESHOLD) {
+        amount += ACCESSORYPRICE;
     }
-  });
+}
+
+amount += calculateTax(amount);
+console.log("Your purchase: " + formatAmount(amount));
+
+if(amount > bankBalance) {
+    console.log("You can afford");
 }
