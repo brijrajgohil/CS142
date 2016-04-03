6f38935503739a32408a123f6a9d9158dfafb67c
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-04-03 13:31:20 +0530

    Goofing with JS

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,29 +1,9 @@
-const SPENDINGTHRESHOLD = 200;
-const TAXRATE = 0.08;
-const PHONEPRICE = 99.99;
-const ACCESSORYPRICE = 9.99;
+var a = 2;
 
-var bankBalance = 303.91;
-var amount = 0;
-
-
-function calculateTax(amount) {
-    return amount * TAXRATE;
-}
-
-function formatAmount(amount) {
-    return "$" + amount.toFixed(2);
-}
-while(amount < bankBalance) {
-    amount += PHONEPRICE;
-    if(amount < SPENDINGTHRESHOLD) {
-        amount += ACCESSORYPRICE;
+(function() {
+    var a  = 3;
+        console.log(a);
     }
-}
+)();
 
-amount += calculateTax(amount);
-console.log("Your purchase: " + formatAmount(amount));
-
-if(amount > bankBalance) {
-    console.log("You can afford");
-}
+console.log(a);
