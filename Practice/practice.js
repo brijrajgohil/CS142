84b54989ee3d437e7d3b82ffd70f40fdf47dbbe4
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-04-24 18:26:51 +0530

    Functions

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,6 +1,10 @@
-function foo(a) {
-    this.a = a;
+function myFunc(routine) {
+    console.log("Called with", routine.toString()); //Will display the function
+    var retVal = routine(10); //Will display 10 and return 11
+    console.log("retVal", retVal);
 }
 
-var bar = new foo(2);
-console.log(bar.a);
+myFunc(function(x) {
+    console.log("Called with ", x);
+    return x + 1;
+});
