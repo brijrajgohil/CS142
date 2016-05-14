f17b8d629cabcca62e34739d6aba4fc42f28ffdb
Brijrajsinh Gohil <brijrajsinhgohil@yahoo.com>
2016-05-15 02:18:16 +0530

    FCC Challenges

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,12 +1,15 @@
 
-function confirmEnding(str, target) {
-  // "Never give up and good luck will find you."
-  // -- Falcor
-  console.log(str.substr(-target.length));
-  if(str.substr(-target.length) == target) {
-      return true;
+function getIndexToIns(arr, num) {
+  // Find my place in this sorted array.
+  var arr1 = arr.sort(function(a, b) {
+      return a - b;
+  });
+  console.log(arr1);
+  for(var i = 0; i < arr1.length; i++) {
+    if(arr[i] > num) {
+        return arr[i-1] == num ? i-1: i;
+    }
   }
-  return false;
 }
 
-confirmEnding("He has to give me a new name", "me");
+console.log(getIndexToIns([5, 3, 20, 3], 5));
